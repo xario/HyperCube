0dcc73f13c74e9982e40ad3664668a0ef5e491f1
akh <git@xar.io>
2022-11-03 21:20:19 +0100

    updated on post data

--- docker/Arduino/hypercube/config.h
+++ docker/Arduino/hypercube/config.h
@@ -797,34 +797,35 @@ void initConfig() {
       return request->send(403);
     }
 
-    const char* operation = index == 0 ? FILE_WRITE : FILE_APPEND;
-    File file = FileFS.open("/data.json", operation);
+    static char buffer[10240];
+
+    //File file = FileFS.open("/data.json", operation);
     if(len) {
-      file.write(data,len);
+      memcpy(buffer + index, data, len);
+      //file.write(data,len);
     }
 
     #ifdef DEBUG
     Serial.println("Receiving data: ");
-    for(size_t i=0; i<len; i++){
+    for (size_t i = 0; i < len; i++) {
       Serial.write(data[i]);
     }
     #endif
 
-    file.close();
-    if(index + len == total){
-      file = FileFS.open("/data.json", "r");
-      String json = file.readString();
+    if (index + len == total) {
+      File file = FileFS.open("/data.json", FILE_WRITE);
+      file.write((uint8_t *)buffer, total);
       file.close();
       #ifdef DEBUG
       Serial.println("JSON: ");
-      Serial.println(json);
+      Serial.println(buffer);
       #endif
-      sides = jsonExtract(json, "sides");
+      sides = jsonExtract(buffer, "sides");
       #ifdef DEBUG
       Serial.println("Sides: ");
       Serial.println(sides);
       #endif
-      String apiKeys = jsonExtract(json, "apiKeys");
+      String apiKeys = jsonExtract(buffer, "apiKeys");
       clockifyApiKeys = jsonExtract(apiKeys, "clockify");
       updateApiUserIds();
       #ifdef DEBUG
